59c768a5384b11fa9f77eeae7beb301a327186a9
unknown <Abby@DESKTOP-2TSGE7G.fareast.corp.microsoft.com>
2022-02-21 17:16:40 -0800

    Order page is updated as per Priyanka Review comments

--- src/main/java/com/qa/OpenCart/Pages/OrderPage.java
+++ src/main/java/com/qa/OpenCart/Pages/OrderPage.java
@@ -1,9 +1,13 @@
 package com.qa.OpenCart.Pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 
+	By order = By.id("order");
+	
 	public void order() {
-		System.out.println("Orderk,");
+		System.out.println("Order id: 12345");
 	}
 	
 }
